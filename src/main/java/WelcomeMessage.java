f637714a7c0ee2f5176b572da202bfa74e694a8c
Khaled Alrusan <khaled66658032@gmail.com>
2023-07-28 03:29:23 -0400

    Fixed the app

--- src/main/java/WelcomeMessage.java
+++ src/main/java/WelcomeMessage.java
@@ -1,19 +1,32 @@
-public class WelcomeMessage {
-    public static void main(String[] args) {
-        String studentName = "Khaled";  // replace with the actual student name
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
 
-        LocalDateTime currentTime = LocalDateTime.now();
-        LocalTime noon = LocalTime.of(12, 0);
-        LocalTime currentLocalTime = currentTime.toLocalTime();
+@WebServlet(name = "WelcomeServlet", urlPatterns = {"/welcome"})
+public class WelcomeMessage extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
 
-        String greeting;
-
-        if (currentLocalTime.isBefore(noon)) {
-            greeting = "Good morning, " + studentName + ", Welcome to COMP367";
+        String partOfDay;
+        if (hour < 12) {
+            partOfDay = "Good morning";
         } else {
-            greeting = "Good afternoon, " + studentName + ", Welcome to COMP367";
+            partOfDay = "Good afternoon";
         }
 
-        System.out.println(greeting);
+        String studentName = request.getParameter("name");
+        if (studentName == null) {
+            studentName = "Student";
+        }
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h1>" + partOfDay + ", " + studentName + ", Welcome to COMP367</h1></body></html>");
+        out.close();
     }
 }
